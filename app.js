ecaf4efdcb635b771bfeba5fed7609db0d3798de
Tyler Healy <tyler_2319@yahoo.com>
2013-02-21 13:03:01 -0500

    Baseline server code
    Allows the following:
    - User Login
    - Creation of new user
    - Get a user's data
    - Create a new todoList item

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ var fs = require("fs");
 // body of a request
 app.use(express.bodyParser());
 
-// The global datastore for this example
-var todoList;
+// The global datastore for user data
+var users;
 
 // Asynchronously read file contents, then call callbackFn
 function readFile(filename, defaultData, callbackFn) {
@@ -39,27 +39,102 @@ function writeFile(filename, data, callbackFn) {
   });
 }
 
-// get all items
-app.get("/todo", function(request, response){
+function getUserData(username) {
+  var defaultList = "[]";
+  var filename = "" + username + ".txt";
+  
+  readFile(filename, defaultList, function(err, data) {
+    userData = JSON.parse(data);
+  });
+  return userData;
+}
+
+function writeUserData(username, userData) {
+  var filename = "" + username + ".txt";
+  writeFile(filename, JSON.stringify(userData));
+}
+
+// login request
+app.get("/login/:user/:pass", function(request, response) {
+  var username = request.params.username;
+  var pass = request.params.pass;
+  var userData = users[username];
+  
+  if (userData !== undefined && userData === pass) {
+    response.send({
+      success: true
+    });
+  } else {
+    response.send({
+      success: false
+    });
+  }
+});
+
+// new user request
+app.post("/new_user/:user/:pass", function(request, response) {
+  var username = request.params.username;
+  var pass = request.params.pass;
+  var data = users[username];
+  
+  if (data !== undefined) {
+    response.send({
+      success: false
+    });
+  } else {
+    users[username] = pass;
+    writeFile("users.txt", JSON.stringify(users));
+    
+    var new_user_data = new Object()
+    new_user_data.username = username;
+    new_user_data.level = 1;
+    new_user_data.powerups = "";
+    new_user_data.total_points = 0;
+    new_user_data.last_login = new Date();
+    new_user_data.high_score = 0;
+    new_user_data.todoList = [];
+    writeUserData(username, new_user_data);
+    
+    response.send({
+      success: true
+    });
+  }
+}
+
+// get a user's todo list
+app.get("/:user/data", function(request, response){
+  var username = request.params.username;
+  var userData = getUserData(username);
+
   response.send({
-    todoList: todoList,
+    userData: userData,
     success: true
   });
 });
 
 // create new item
-app.post("/todo", function(request, response) {
-  todoList.push({"desc": request.body.desc,
-                 "completed": false});
-  writeFile("data.txt", JSON.stringify(todoList));
+app.post("/:user/todo", function(request, response) {
+  var username = request.params.username;
+  var userData = getUserData(username);
+
+  userData.todoList.push({
+    "name": request.body.name,
+    "priority": request.body.priority,
+    "due_date": request.body.due_date,
+    "desc": request.body.desc,
+    "timestamp": new Date(),
+    "completed": false
+  });
+  
+  writeUserData(username, userData);
   response.send({
-    todoList: todoList,
+    userData: userData,
     success: true
   });
 });
 
 // update one item
-app.put("/todo/:id", function(request, response){
+app.put("/:user/todo/:id", function(request, response){
   // change todo at index, to the new todo
   var id = request.params.id;
   var todo = { "desc": request.body.desc,
@@ -72,18 +147,8 @@ app.put("/todo/:id", function(request, response){
   });
 });
 
-// delete entire list
-app.delete("/todo", function(request, response){
-  todoList = [];
-  writeFile("data.txt", JSON.stringify(todoList));
-  response.send({
-    todoList: todoList,
-    success: true
-  });
-});
-
 // delete one item
-app.delete("/todo/:id", function(request, response){
+app.delete("/:user/todo/:id", function(request, response){
   var id = request.params.id;
   todoList.splice(id, 1);
   writeFile("data.txt", JSON.stringify(todoList));
@@ -101,8 +166,8 @@ app.get("/static/:staticFilename", function (request, response) {
 function initServer() {
   // When we start the server, we must load the stored data
   var defaultList = "[]";
-  readFile("data.txt", defaultList, function(err, data) {
-    todoList = JSON.parse(data);
+  readFile("users.txt", defaultList, function(err, data) {
+    users = JSON.parse(data);
   });
 }
 
